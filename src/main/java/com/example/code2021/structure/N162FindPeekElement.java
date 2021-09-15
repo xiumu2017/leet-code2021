81a92b0475a5eb32299dd54e3bc6917e7ced037f
Paradise <dz.paradis3@gmail.com>
2021-09-15 19:21:57 +0800

    2021-9-15

---
+++ src/main/java/com/example/code2021/structure/N162FindPeekElement.java
@@ -0,0 +1,55 @@
+package com.example.code2021.structure;
+
+class N162FindPeekElement {
+    public static void main(String[] args) {
+        N162FindPeekElement element = new N162FindPeekElement();
+        final int peakElement = element.findPeakElement(new int[]{1, 2, 3, 1});
+        System.out.println(peakElement);
+    }
+
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        int ans = -1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (compare(nums, mid - 1, mid) < 0 && compare(nums, mid, mid + 1) > 0) {
+                ans = mid;
+                break;
+            }
+            if (compare(nums, mid, mid + 1) < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    // 辅助函数，输入下标 i，返回一个二元组 (0/1, nums[i])
+    // 方便处理 nums[-1] 以及 nums[n] 的边界情况
+    public int[] get(int[] nums, int idx) {
+        if (idx == -1 || idx == nums.length) {
+            return new int[]{0, 0};
+        }
+        return new int[]{1, nums[idx]};
+    }
+
+    public int compare(int[] nums, int idx1, int idx2) {
+        int[] num1 = get(nums, idx1);
+        int[] num2 = get(nums, idx2);
+        if (num1[0] != num2[0]) {
+            return num1[0] > num2[0] ? 1 : -1;
+        }
+        if (num1[1] == num2[1]) {
+            return 0;
+        }
+        return num1[1] > num2[1] ? 1 : -1;
+    }
+}
+
+//作者：LeetCode-Solution
+//链接：https://leetcode-cn.com/problems/find-peak-element/solution/xun-zhao-feng-zhi-by-leetcode-solution-96sj/
+//来源：力扣（LeetCode）
+//著作权归作者所有。商业转载 请联系作者获得授权，非商业转载请注明出处。
