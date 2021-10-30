b503642ec06c2bacea339da2ebc405f624ec9ed6
Paradise <zdparadise@icloud.com>
2021-10-30 23:10:10 +0800

    2021-10-30

---
+++ src/main/java/com/example/code2021/structure/oct/N260SingleNumberIII.java
@@ -0,0 +1,28 @@
+package com.example.code2021.structure.oct;
+
+class N260SingleNumberIII {
+
+    public int[] singleNumber(int[] nums) {
+        int xorsum = 0;
+        for (int num : nums) {
+            xorsum ^= num;
+        }
+        // 防止溢出
+        int lsb = (xorsum == Integer.MIN_VALUE ? xorsum : xorsum & (-xorsum));
+        int type1 = 0;
+        int type2 = 0;
+        for (int num : nums) {
+            if ((num & lsb) != 0) {
+                type1 ^= num;
+            } else {
+                type2 ^= num;
+            }
+        }
+        return new int[]{type1, type2};
+    }
+}
+
+//作者：LeetCode-Solution
+//链接：https://leetcode-cn.com/problems/single-number-iii/solution/zhi-chu-xian-yi-ci-de-shu-zi-iii-by-leet-4i8e/
+//来源：力扣（LeetCode）
+//著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
