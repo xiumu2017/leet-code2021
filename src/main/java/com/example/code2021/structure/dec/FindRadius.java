5d1e2a0559dcc864bd69dd6d8c5b7f2e1d9d61db
xiumu <zdparadise@icloud.com>
2021-12-20 22:56:19 +0800

    2021-12-20

---
+++ src/main/java/com/example/code2021/structure/dec/FindRadius.java
@@ -0,0 +1,25 @@
+package com.example.code2021.structure.dec;
+
+import java.util.Arrays;
+
+class FindRadius {
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        int ans = 0;
+        for (int i = 0, j = 0; i < houses.length; i++) {
+            int curDistance = Math.abs(houses[i] - heaters[j]);
+            while (j < heaters.length - 1 && Math.abs(houses[i] - heaters[j]) >= Math.abs(houses[i] - heaters[j + 1])) {
+                j++;
+                curDistance = Math.min(curDistance, Math.abs(houses[i] - heaters[j]));
+            }
+            ans = Math.max(ans, curDistance);
+        }
+        return ans;
+    }
+}
+
+//作者：LeetCode-Solution
+//链接：https://leetcode-cn.com/problems/heaters/solution/gong-nuan-qi-by-leetcode-solution-rwui/
+//来源：力扣（LeetCode）
+//著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
