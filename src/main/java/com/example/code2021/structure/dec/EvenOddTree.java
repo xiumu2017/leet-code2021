996d0514d2295b0975927a031727b045c8dc8a8d
Paradise <zdparadise@icloud.com>
2021-12-25 22:43:25 +0800

    2021-12-25

---
+++ src/main/java/com/example/code2021/structure/dec/EvenOddTree.java
@@ -0,0 +1,62 @@
+package com.example.code2021.structure.dec;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+class EvenOddTree {
+    public boolean isEvenOddTree(TreeNode root) {
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        int level = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int prev = level % 2 == 0 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                int value = node.val;
+                // 判断奇偶性
+                if (level % 2 == value % 2) {
+                    return false;
+                }
+                // 判断单调性
+                if ((level % 2 == 0 && value <= prev) || (level % 2 == 1 && value >= prev)) {
+                    return false;
+                }
+                prev = value;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            level++;
+        }
+        return true;
+    }
+
+    //      Definition for a binary tree node.
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+
+//作者：LeetCode-Solution
+//链接：https://leetcode-cn.com/problems/even-odd-tree/solution/qi-ou-shu-by-leetcode-solution-l7bw/
+//来源：力扣（LeetCode）
+//著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
